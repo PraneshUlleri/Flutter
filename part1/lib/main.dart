ba15b07b2a3eea468ab19991a6bfd171bcb618f3
Pranesh <ipraneshulleri@gmail.com>
2019-02-27 23:59:31 +0530

    something new

---
+++ part1/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+    final wordPair = new WordPair.random();
+    return MaterialApp(
+      /*title: 'Welcome to Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),*/title: 'Welcome to Flutter',
+        home: new Scaffold(
+        appBar: new AppBar(
+        title: new Text('Welcome to Flutter'),
+    ),
+      body: new Center(
+        child: new Text(wordPair.asPascalCase),
+      ),
+        )
+    );
+
+}
+}
