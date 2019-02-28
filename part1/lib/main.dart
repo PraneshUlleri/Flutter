deb3a96d3f7a482d5dfea73119a01d9e94c277d8
Pranesh <ipraneshulleri@gmail.com>
2019-02-28 13:21:58 +0530

    check1

--- part1/lib/main.dart
+++ part1/lib/main.dart
@@ -3,25 +3,32 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-
-    final wordPair = new WordPair.random();
-    return MaterialApp(
-      /*title: 'Welcome to Flutter',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-      ),*/title: 'Welcome to Flutter',
+    return new MaterialApp(
+     title: 'Welcome to Flutter',
         home: new Scaffold(
         appBar: new AppBar(
-        title: new Text('Welcome to Flutter'),
-    ),
-      body: new Center(
-        child: new Text(wordPair.asPascalCase),
-      ),
-        )
-    );
+        title: const Text('Welcome to Flutter'),
+     ),
 
+         body: new Center(
+           child: new RandomWords(),
+          ),
+        ),
+    );
 }
+}
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
+
+class RandomWordState extends State<RandomWords>{
+  @override
+  Widget build(BuildContext context) {
+    final WordPair wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
+  }
 }
