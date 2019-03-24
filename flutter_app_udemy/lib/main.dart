12411ea1277b62f8c341ca43815721ea0bb1871a
Pranesh <ipraneshulleri@gmail.com>
2019-03-24 14:51:44 +0530

    added appbar

--- flutter_app_udemy/lib/main.dart
+++ flutter_app_udemy/lib/main.dart
@@ -5,15 +5,19 @@ void main(){
 
     new MaterialApp(
       title: "My app:",
-      home: new Material(
-      color:Colors.lightBlue,
-        child:Center(
-          child:Text(
+      home: Scaffold(
+        appBar: AppBar(title:Text("My first App Screen"),),
+        body:new Material(
+          color:Colors.lightBlue,
+          child:Center(
+            child:Text(
               "Hello World",
-              textDirection: TextDirection.ltr
+              textDirection: TextDirection.ltr,
+              style: TextStyle(color:Colors.white,fontSize: 20.0),
+            ),
           ),
-          ),
-      ),
+        ),
+      )
     )
   );
 }
